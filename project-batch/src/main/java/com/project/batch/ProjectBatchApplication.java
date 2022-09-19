5d8b2bf69abf43e294ed5f3f5b7e88b2e837be18
jhlee <junhui12345@gmail.com>
2022-09-19 21:39:20 +0900

    배치 스케쥴러 등록

--- project-batch/src/main/java/com/project/batch/ProjectBatchApplication.java
+++ project-batch/src/main/java/com/project/batch/ProjectBatchApplication.java
@@ -3,9 +3,12 @@ package com.project.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableBatchProcessing
+
 @SpringBootApplication
+@EnableBatchProcessing
+@EnableScheduling
 public class ProjectBatchApplication {
 
     public static void main(String[] args) {
